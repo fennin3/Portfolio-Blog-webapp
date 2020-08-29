515855d4c6d7c59c6c64430dacca4234d9956051
Ennin Francis <enninfrancis47@gmail.com>
2020-08-29 20:38:09 +0000

    Settings updated

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -99,11 +99,11 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
-COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
+# COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
