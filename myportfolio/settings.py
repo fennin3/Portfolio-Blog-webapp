948e0a706f8d2c9b171b8074638533d2cce321cf
fennin3 <enninfrancis47@gmail.com>
2020-10-16 19:09:34 +0000

    make it better

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hrj48*mobkzo)s6q+2wfqcg=_=@5c38n&sb4_vlp#bb^zyp_pi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'rennintech.herokuapp.com', 'rennintech.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'rennintech.herokuapp.com', 'rennintech.com', 'www.rennintech.com', 'www.rennintech.herokuapp.com']
 
 
 # Application definition
